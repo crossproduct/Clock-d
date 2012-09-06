ca496e25059b56416b93e4be25a5150d46b0a048
Chris Ross <chris.ross@controlgroup.com>
2012-09-06 10:02:35 -0400

    comment out the jquery

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
 <head>
 <title>Clock'd | A Control Group Experience</title>
 	<link rel="stylesheet" href="css/style.css" />
+	<!--
 	<script type="text/javascript" src="js/jquery.min.js"></script>
-        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-        <script type="text/javascript" src="js/functions.js"></script>
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	-->
+    <script type="text/javascript" src="js/functions.js"></script>
 </head>
 <body>
 	<div id="canvas_container" align="center"><canvas id="clock_canvas"/></div>
