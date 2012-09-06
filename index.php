3d662d6429bc7e8b97dac22520190f5fc32f76c0
Chris Ross <chris.ross@controlgroup.com>
2012-09-06 10:03:23 -0400

    uncomment out the jquery

--- index.php
+++ index.php
@@ -6,10 +6,8 @@
 <head>
 <title>Clock'd | A Control Group Experience</title>
 	<link rel="stylesheet" href="css/style.css" />
-	<!--
 	<script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-	-->
     <script type="text/javascript" src="js/functions.js"></script>
 </head>
 <body>
