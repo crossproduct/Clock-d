56d2860bc5022da7c88fc721187918558691727d
Chris Ross <chris.ross@controlgroup.com>
2012-09-09 21:49:35 -0400

    updated readme and weather infomration display

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 Clock-d
 =======
 
-A soothing rhythmic clock.
+A minimal but beautiful, soothing rhythmic clock with daily weather updates. 
+
+* Rhythmic pulsing on the second for a soothing zen like trance.
+* Dynamic daytime / night time transition with sunrise and sunset color transitions
+* Weather updates on the hour and displays info to user. A click on the user interface also reveals the weather dashboard.
+* Sunrise, sunset, high/low/current tempurature, and humidity.
+
+* Weather service provided by WeatherBug.com
--- index.php
+++ index.php
@@ -13,6 +13,17 @@
 <body>
 	<div id="canvas_container" align="center"><canvas id="clock_canvas"/></div>
 	<div id="infomatics">
+		<div id="temperatureInfo">
+			<span id="temperature_label"><b></b></span><br/>
+			<span id="temperatureHigh_text">0&deg;</span><span style="font-size:16px">High</span><br/>
+			<span id="temperatureCurrent_text">0&deg;</span><span style="font-size:25px">Current</span><br/>
+			<span id="temperatureLow_text">0&deg;</span><span style="font-size:16px">Low</span>
+		</div>
+		<div id="humidityInfo">
+			<span id="humidity_label"><b>Humidity</b></span><br/>
+			<span id="humidity_text">0</span><span id="humidity_units" style="font-size:20px">%</span>
+		</div>
+
 		<div id="sunriseInfo">
 			<span id="sunrise_label"><b>Sunrise</b></span><br/>
 			<span id="sunrise_text">&nbsp;</span><span style="font-size:20px">am</span>
