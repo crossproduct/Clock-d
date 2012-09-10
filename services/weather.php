a3322e741573532b3446bbd103926f6efd5b3b21
Chris Ross <chris.ross@controlgroup.com>
2012-09-09 20:01:47 -0400

    update api key for weather service and updated transition animation colors for sunset sunrise

--- services/weather.php
+++ services/weather.php
@@ -1,6 +1,6 @@
 <?php
 	/* This is a proxy service for connecting to WeatherBug APIs, gets the weather for a given LAT,LONG */
-	$url = 'http://i.wxbug.net/REST/Direct/GetObs.ashx?la='.$_GET['LAT'].'&lo='.$_GET['LONG'].'&&ic=1&api_key=jwp2wjpfnuku7u64csy5x827';
+	$url = 'http://i.wxbug.net/REST/Direct/GetObs.ashx?la='.$_GET['LAT'].'&lo='.$_GET['LONG'].'&&ic=1&api_key=nqy5h7gsw63jk2btrtxgd4vw';
 	$data = file_get_contents($url);
     echo $_GET['callback'] . '({data:' . $data . '})';
 ?>
